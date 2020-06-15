72b339601b6649ce6ef59bca6dfbdf0f7e12b165
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2020-06-15 16:24:21 +0700

    Implemented the emergency notice

--- src/components/EmergencyNotice.vue
+++ src/components/EmergencyNotice.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>EmergencyNotice</div>
+  <div id="emergency-notice">
+    <div class="container text-center py-4">
+      <span class="text-dark h1">AFTER HOURS EMERGENCY?</span>
+      <span class="text-white h1 ml-4">PH 4779 5222</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +12,9 @@ export default {
   name: 'EmergencyNotice'
 }
 </script>
+
+<style lang="scss" scoped>
+#emergency-notice {
+  background-color: #ff7b00;
+}
+</style>
