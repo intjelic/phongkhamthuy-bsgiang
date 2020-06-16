85e5558938e9721f1a7c3198694927d5adc73950
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2020-06-16 12:43:32 +0700

    Installed vue2-google-maps package

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import 'bootstrap'
 import '@/assets/style.scss'
 import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
 import Website from './Website.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyA257_ta9LCveTnvVe6GFfqLUtTDquTErQ',
+    libraries: 'visualization'
+  }
+})
 
 new Vue({
   router,
