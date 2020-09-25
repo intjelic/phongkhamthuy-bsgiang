df923f0d6df0cf0841ff81d800a78e0a5f27e669
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2020-09-26 01:54:05 +0700

    Implemented a draft of the news and shop pages

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="news">
-    <page-header />
-    <lorem-ipsum />
+    <page-header
+      :title="$t('navigationBar.news')"
+      :description="$t('pageHeaders.descriptions.news')"
+    />
+    <div class="container">
+      <br>
+      <page-introduction class="my-4 w-75 mx-auto" :text="$t('newsPage.introduction')"/>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <div class="px-3 text-danger h5 text-center">
+        Sorry! This page is still under development and this page isn't completed yet. Please come back later.
+      </div>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+    </div>
     <emergency-notice />
     <page-footer />
   </div>
@@ -9,17 +31,17 @@
 
 <script>
 import PageHeader from '@/components/PageHeader.vue'
+import PageIntroduction from '@/components/PageIntroduction.vue'
 import EmergencyNotice from '@/components/EmergencyNotice.vue'
 import PageFooter from '@/components/PageFooter.vue'
-import LoremIpsum from '@/components/LoremIpsum.vue'
 
 export default {
   name: 'News',
   components: {
     PageHeader,
+    PageIntroduction,
     EmergencyNotice,
-    PageFooter,
-    LoremIpsum
+    PageFooter
   }
 }
 </script>
--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="shop">
-    <page-header />
-    <lorem-ipsum />
+    <page-header
+      :title="$t('navigationBar.shop')"
+      :description="$t('pageHeaders.descriptions.shop')"
+    />
+    <div class="container">
+      <br>
+      <page-introduction class="my-4 w-75 mx-auto" :text="$t('shopPage.introduction')"/>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <div class="px-3 text-danger h5 text-center">
+        Sorry! This page is still under development and this page isn't completed yet. Please come back later.
+      </div>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+    </div>
     <emergency-notice />
     <page-footer />
   </div>
@@ -9,17 +31,17 @@
 
 <script>
 import PageHeader from '@/components/PageHeader.vue'
+import PageIntroduction from '@/components/PageIntroduction.vue'
 import EmergencyNotice from '@/components/EmergencyNotice.vue'
 import PageFooter from '@/components/PageFooter.vue'
-import LoremIpsum from '@/components/LoremIpsum.vue'
 
 export default {
   name: 'Shop',
   components: {
     PageHeader,
+    PageIntroduction,
     EmergencyNotice,
-    PageFooter,
-    LoremIpsum
+    PageFooter
   }
 }
 </script>
