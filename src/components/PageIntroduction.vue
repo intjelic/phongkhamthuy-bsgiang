f8ef51066aa11037a067395397f4e9edefc18d65
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2020-09-26 01:52:48 +0700

    Implemented PageIntroduction component

---
+++ src/components/PageIntroduction.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="d-flex border-top border-bottom">
+      <div><img src="@/assets/help-circle.svg" style="margin-top: 50px;"></div>
+      <div class="flex-grow-1 h5 px-5 py-3 text-center">{{ text }}</div>
+      <div><img src="@/assets/help-circle.svg" style="margin-top: 50px;"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['text']
+}
+</script>
+
+<style>
+
+</style>
