2ad74639582bdd33bef5ce22b563d5b91d206400
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2020-09-26 02:03:34 +0700

    Implemented a draft of the home page carousel

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -1,19 +1,40 @@
 <template>
-  <div style="height: 550px;">
+  <div style="height: 450px;">
     <div v-if="$mq == 'desktop'">
       <div id="background">
         <div id="right-square"></div>
       </div>
       <div id="foreground" class="container">
-        <img
-          id="image"
-          src="https://palmsvetgroup.com.au/wp-content/uploads/2015/07/slider-puppies.jpg"
-        />
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="https://palmsvetgroup.com.au/wp-content/uploads/2015/07/slider-puppies.jpg" class="d-block w-100" height="350px" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="https://palmsvetgroup.com.au/wp-content/uploads/2015/07/slider-puppies.jpg" class="d-block w-100" height="350px" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="https://palmsvetgroup.com.au/wp-content/uploads/2015/07/slider-puppies.jpg" class="d-block w-100" height="350px" alt="...">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
       </div>
     </div>
     <div v-else>
-      <div class="bg-primary" style="height: 225px;"></div>
-      <div class="bg-secondary" style="height: 225px;"></div>
+      <div class="bg-primary" style="height: 225px;">
+        Left
+      </div>
+      <div class="bg-secondary" style="height: 225px;">
+        Right
+      </div>
     </div>
   </div>
 </template>
@@ -28,14 +49,14 @@ export default {
 #background {
   position: absolute;
   width: 100%;
-  height: 550px;
+  height: 450px;
   background-color: rgb(21, 105, 142);
 }
 
 #right-square {
   width: 50%;
   left: 50%;
-  height: 550px;
+  height: 450px;
   background-color: rgb(37, 170, 227);
 }
 
