dfb962135f995a30adb5b04550d60a07f38412d7
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2020-09-26 02:09:03 +0700

    Updated and added missing translations (en.js and vi.js)

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -6,6 +6,30 @@ const en = {
     news: 'News',
     shop: 'Shop'
   },
+  pageHeaders: {
+    descriptions: {
+      home: 'Description of the Home page.',
+      services: 'List of our veterinary services.',
+      questions: 'Answers to very common questions.',
+      news: 'Check out the latest news about our clinic.',
+      shop: 'Buy and get delivered your pet accessories and food.'
+    }
+  },
+  homePage: {
+    // Nothing for now...
+  },
+  servicesPage: {
+    introduction: 'Because services are diverse and different across  each veterinarian clinics, we have made a list. Please, reach us out for any inquiry about prices.'
+  },
+  questionsPage: {
+    introduction: 'As concerned and cautious pet owners, there might be questions about this clinic. We have compiled a list of answers to common questions. Feel free to contact us with the phone number or via email.'
+  },
+  newsPage: {
+    introduction: "Our clinic is involved in various outside activities such as raising awareness about animal cruelty, education of the next generation, and volunteering events for people who can't always afford health care for their pets. And sometimes we just grow. Keep yourself updated."
+  },
+  shopPage: {
+    introduction: 'We have developed an online shop where our clients can buy their pet accessories, food and cie. Our profit margin goes directly toward the development of our clinic. Consider supplying yourself here for convenience and development of the animal healthcare in Vietnam.'
+  },
   emergencyNotice: {
     text: 'ERMEGENCY HOTLINE',
     phoneNumber: '+84 919 250 389'
--- src/i18n/vi.js
+++ src/i18n/vi.js
@@ -6,9 +6,35 @@ const vi = {
     news: 'News',
     shop: 'Shop'
   },
+  pageHeaders: {
+    descriptions: {
+      home: 'Description of the Home page.',
+      services: 'Danh sách các dịch vụ thú y của chúng tôi.',
+      questions: 'Câu trả lời cho những câu hỏi rất phổ biến.',
+      news: 'Kiểm tra các tin tức mới nhất về phòng khám của chúng tôi.',
+      shop: 'Mua và nhận các phụ kiện và thức ăn cho thú cưng của bạn.'
+    }
+  },
+  homePage: {
+    // Nothing for now...
+  },
+  servicesPage: {
+    introduction: 'Bởi vì các dịch vụ rất đa dạng và khác nhau ở mỗi phòng khám bác sĩ thú y, chúng tôi đã lập một danh sách. Vui lòng liên hệ với chúng tôi nếu có bất kỳ yêu cầu nào về giá cả.'
+  },
+  questionsPage: {
+    introduction: 'Là chủ sở hữu vật nuôi quan tâm và thận trọng, có thể có câu hỏi về phòng khám này. Chúng tôi đã biên soạn một danh sách các câu trả lời cho các câu hỏi phổ biến. Vui lòng liên hệ với chúng tôi qua số điện thoại hoặc qua email.'
+  },
+  newsPage: {
+    introduction: 'Là chủ sở hữu nuôi quan tâm và quan trọng, có thể có câu hỏi về phòng khám này. Chúng tôi đã biên soạn một danh sách các câu trả lời cho các câu hỏi phổ biến. Vui lòng liên hệ với chúng tôi, tôi qua số điện thoại hoặc qua email.'
+  },
+  shopPage: {
+    introduction: 'Chúng tôi đã phát triển một cửa hàng trực tuyến, nơi khách hàng của chúng tôi có thể mua phụ kiện, thức ăn và bánh cho thú cưng của họ. Tỷ suất lợi nhuận của chúng tôi phụ thuộc trực tiếp vào sự phát triển của phòng khám. Hãy cân nhắc việc tự cung cấp cho mình tại đây để thuận tiện và phát triển ngành thú y tại Việt Nam.'
+  },
   emergencyNotice: {
     text: 'AFTER HOURS EMERGENCY?',
     phoneNumber: 'PH 4779 5222'
+  },
+  pageFooter: {
   }
 }
 
